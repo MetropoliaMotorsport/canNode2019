1281835f1e79fe84fde55ce894237fe80da4d9e1
pyrophorol <scott.threet@metropolia.fi>
2019-07-07 10:21:31 +0300

    fixing can to work on PB8 and PB9

--- Src/stm32f1xx_hal_msp.c
+++ Src/stm32f1xx_hal_msp.c
@@ -100,17 +100,19 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
 	if(hcan->Instance==hcan1.Instance)
 	{
 		__HAL_RCC_CAN1_CLK_ENABLE();
-		//__HAL_RCC_GPIOA_CLK_ENABLE();
+		__HAL_RCC_GPIOB_CLK_ENABLE();
 
-		GPIO_InitStruct.Pin = GPIO_PIN_11;
+		GPIO_InitStruct.Pin = GPIO_PIN_8;
 		GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
 		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-		GPIO_InitStruct.Pin = GPIO_PIN_12;
+		GPIO_InitStruct.Pin = GPIO_PIN_9;
 		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
 		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+		__HAL_AFIO_REMAP_CAN1_2();
 
 		HAL_NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 0, 0);
 		HAL_NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
@@ -126,7 +128,7 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* hcan)
 	if(hcan->Instance==hcan1.Instance)
 	{
 		__HAL_RCC_CAN1_CLK_DISABLE();
-		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+		HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8|GPIO_PIN_9);
 		HAL_NVIC_DisableIRQ(USB_LP_CAN1_RX0_IRQn);
 	}
 	else
